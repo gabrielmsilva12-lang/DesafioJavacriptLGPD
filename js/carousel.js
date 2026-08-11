03596d2890fb32a430e58509a673e35ce0161506
gabrielmsilva12-lang <gabriel.m.silva12@ba.estudante.senai.br>
2026-08-11 10:07:07 -0300

    Alteração no onclick

--- js/carousel.js
+++ js/carousel.js
@@ -21,7 +21,7 @@ class Carousel {
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
                 Carousel.Next(); //start
-               // Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
+               Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
             }
             
         } else {
